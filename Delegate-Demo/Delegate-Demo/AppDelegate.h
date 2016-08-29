356f7cce25089e82df09737c94d67bcba55eae50
LiuXiaoZhuang <2046703959@qq.com>
2016-08-29 21:39:38 +0800

    初始化项目

---
+++ Delegate-Demo/Delegate-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Delegate-Demo
+//
+//  Created by 刘小壮 on 16/8/29.
+//  Copyright © 2016年 刘小壮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
